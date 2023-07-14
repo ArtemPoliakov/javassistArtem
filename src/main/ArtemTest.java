3036d0a83a8c41d013d1e883cc013b4541840dcb
ArtemPoliakov <apoliakov50@gmail.com>
2023-07-14 12:53:44 +0300

    Cherry-Pick 2

--- src/main/ArtemTest.java
+++ src/main/ArtemTest.java
@@ -1,3 +1,11 @@
 public class ArtemTest {
     //this class was added to test Github and learn how to use it
+
+    //this is a third commit and it is in a master branch :)
+
+    // one more ok
+
+    // one more addon to the master
+
+    //and again)
 }
